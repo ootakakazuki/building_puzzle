f0135b00e302d4cda3dc28dc0521c838dd309060
kuro <lllll1llllll1llll1@yahoo.co.jp>
2021-02-07 16:37:16 +0900

    fix

--- hoge.c
+++ hoge.c
@@ -5,13 +5,13 @@ int g_down_box[4];
 int g_left_box[4];
 int g_right_box[4];
 char field[4][4];
-
+int seg[4] = {8, 16, 24, 31};
 
 int		from_up_check(int j)
 {
-	int		k = 0;
-	int		max_count = 0;
-	int		pro_val = (field[3][j] - '0');
+	int		k = 1;
+	int		max_count = 1;
+	int		pro_val = (field[0][j] - '0');
 	while (k <= 3)
 	{
 		if (pro_val < (field[k][j] - '0'))
@@ -28,7 +28,7 @@ int		from_up_check(int j)
 int		from_down_check(int j)
 {
 	int		k = 2;
-	int		max_count = 0;
+	int		max_count = 1;
 	int		pro_val = (field[3][j] - '0');
 	while (k <= 3)
 	{
@@ -45,9 +45,9 @@ int		from_down_check(int j)
 
 int		from_left_check(int i)
 {
-	int		k = 0;
-	int		max_count = 0;
-	int		pro_val = (field[i][3] - '0');
+	int		k = 1;
+	int		max_count = 1;
+	int		pro_val = (field[i][0] - '0');
 	while (k <= 3)
 	{
 		if (pro_val < (field[i][k] - '0'))
@@ -64,7 +64,7 @@ int		from_left_check(int i)
 int		from_right_check(int i)
 {
 	int		k = 2;
-	int		max_count = 0;
+	int		max_count = 1;
 	int		pro_val = (field[i][3] - '0');
 	while (k >= 0)
 	{
@@ -93,13 +93,13 @@ int		row_check(int num, int i, int j)
 	return 1;
 }
 
-int		col_check(int num, int i)
+int		col_check(int num, int i, int j)
 {
 	int 	i_tmp = 0;
 
-	while (i_tmp < 3)
+	while (i_tmp < i)
 	{
-		if (field[i_tmp][3] == num) return 0;
+		if (field[i_tmp][j] == num) return 0;
 		i_tmp++;
 	}
 	return 1;
@@ -108,17 +108,25 @@ int		col_check(int num, int i)
 void	print_field()
 {
 	int 	i = 0;
-	int 	j = 0;
 
 	while (i < 4)
 	{
+		int j = 0;
 		while (j < 4)
 		{
-			(j == 3) ? printf("%c\n", field[i][j]) : printf("%c", field[i][j]);;
+			if (j == 3)
+			{
+				printf("%d\n", field[i][j]);
+			}
+			else
+			{
+				 printf("%d ", field[i][j]);
+			}
 			j++;
 		}
 		i++;
 	}
+	printf("\n");
 }
 
 // 再帰を利用して数を入力
@@ -127,27 +135,38 @@ int		back_track(int i, int j)
 	int		num = 1;
 
 	if (i == 4) {
+		printf("pass");
 		print_field();
 		return 1;
 	}
 	
 	while (num < 5)
 	{
-		if (!row_check(num, i, j))continue;
-		field[i][j] = num;	
-		if (j == 3 && !from_right_check(i) && !from_left_check(i) && back_track(i + 1, 0))
+		if (!row_check(num, i, j) || !col_check(num, i, j))
 		{
-			field[i][j] = 0;	
+			num++;
+			continue;
 		}
-		if (i == 3 && !col_check(num, i) && !from_up_check(j) && !from_down_check(j) && back_track(i, j + 1))
+		field[i][j] = num;
+		print_field();
+		if (j == 3 && from_right_check(i) && from_left_check(i) && back_track(i + 1, 0))
 		{
-			field[i][j] = 0;
+			return 1;
+			//field[i][j] = 0;	
 		}
-		if (back_track(i, j + 1))
+		else if (i == 3 && !col_check(num, i, j) && from_up_check(j) && from_down_check(j) && back_track(i, j + 1))
 		{
-			field[i][j] = 0;	
+			return 1;
+			//field[i][j] = 0;
 		}
-		
+		else 
+		{
+			if (back_track(i, j + 1))
+			{
+				return 1;
+			}
+		}
+		field[i][j] = 0;	
 		num++;
 	}
 	return 0;
@@ -169,36 +188,58 @@ int		char_len(char *str)
 		i++;
 		re++;
 	}
+	printf("re=%d \n", re);
 	return re;
 }
 
 
 int		main(int argc, char **argv)
 {
+
 	int		i = 0;
-	int a = char_len(*argv);
-	if (char_len(*argv) != 31)
+	int 	j = 0;
+	int a = char_len(argv[1]);
+	if (a != 31)
 	{
+		// 31文字ないときに出力
 		printf("%d%s",a, "ERROR1");
 		return 0;
 	}
-
-	/*
-	while (i < 32)
-	(
-		argv[i]
-		
-		if (i % 2 == 0 && )
+	while (i < 4)
+	{
+		while (j < seg[i])
 		{
-			
+			int m = (j % 8) / 2;
+			if (j % 2 == 0)
+			{
+				if (i == 0)
+				{
+					g_up_box[m] = (argv[1][j] - '0');
+				}
+				else if (i == 1)
+				{
+					g_down_box[m] = (argv[1][j] - '0');
+				}
+				else if (i == 2)
+				{
+					g_left_box[m] = (argv[1][j] - '0');
+				}
+				else
+				{
+					g_right_box[m] = (argv[1][j] - '0');
+				}
+			}
+			j++;
 		}
 		i++;
-	)
-	*/
+	}	
 	if (!back_track(0, 0))
 	{
-		printf("%s", "ERROR2");
+		// 与えられた引数で解がでないときに出力
+		printf("%s", "ERROR2\n");
 	}
+	
+	
 	return 0;
 }
 
